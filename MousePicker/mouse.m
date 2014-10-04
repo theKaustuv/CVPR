d211248331e394c3bd78e1d706bb38376aaab00d
spiderhacker <ayan.rockstar1@gmail.com>
2014-10-04 19:39:50 +0530

    Almost done
    Storing the selected part pending

--- MousePicker/mouse.m
+++ MousePicker/mouse.m
@@ -17,32 +17,40 @@ sizeHW = size(image);
 sizeH = sizeHW(1);
 sizeW = sizeHW(2);
 
-% show image on imshow
-imshow(image)
+while 1
+    % show image on imshow
+    imshow(image)
 
-% wait for two clicks & get the position info
-recorded = ginput(2);   % the recorded click-info
-pos1x = floor(recorded(1,1));  % 1st position - X coordinate
-pos1y = floor(recorded(1,2));  % 1st position - Y coordinate
-pos2x = floor(recorded(2,1));  % 2nd position - X coordinate
-pos2y = floor(recorded(2,2));  % 2nd position - Y coordinate
+    % wait for two clicks & get the position info
+    recorded = ginput(2);   % the recorded click-info
+    pos1x = floor(recorded(1,1));  % 1st position - X coordinate
+    pos1y = floor(recorded(1,2));  % 1st position - Y coordinate
+    pos2x = floor(recorded(2,1));  % 2nd position - X coordinate
+    pos2y = floor(recorded(2,2));  % 2nd position - Y coordinate
 
-% Selected area validity check
-if ((pos2x>pos1x) && (pos2y>pos1y))
-    disp('Its alright');
-else
-    disp('Try to drag from upper-left to lower-bottom');
-end
+    % Selected area validity check
+    if ((pos2x>pos1x) && (pos2y>pos1y))
+        %disp('Its alright');
+        selected = zeros(pos2y - pos1y,pos2x - pos1x);
+        col = 1;
+        row = 1;
+        for rowwise = pos1x:pos2x
+            for colwise = pos1y:pos2y
+                selected(col,row) = image(colwise,rowwise);
+                col = col+1;
+            end
+            row = row + 1;
+            col = 1;
+        end
+        imshow(selected)
+        ask = input('Enter the language : ','s');
+        % DO SOMETHING FOR STORING THE SELECTED PART
 
-selected = zeros(pos2y - pos1y,pos2x - pos1x);
+        
 
-for rowwise = pos1x:pos2x
-    for colwise = pos1y:pos2y
-        selected(col,row) = image(colwise,rowwise);
-        col = col+1;
+        % DO SOMETHING FOR STORING THE SELECTED PART
+        clear selected
+    else
+        disp('Try to drag from upper-left to lower-bottom');
     end
-    row = row + 1;
-    col = 1;
 end
-
-imshow(selected)
